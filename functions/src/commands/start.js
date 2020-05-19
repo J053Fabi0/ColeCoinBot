edea556ea234f964afab287164d8b790f5748312
J053Fabi0 <arguellojf1@gmail.com>
2020-05-19 14:45:10 -0600

    Detecta si es un grupo y no manda el menú

--- functions/src/commands/start.js
+++ functions/src/commands/start.js
@@ -23,29 +23,32 @@ module.exports = bot => {
 }
 
 function start(ctx, shouldEdit) {
-  const { startMessage } = require('../messages/messages');
+  let chat_type = ctx.update.message.chat.type;
+  if (chat_type == "private") {
+    const { startMessage } = require('../messages/messages');
 
-  let buttons = {
-    reply_markup: {
-      inline_keyboard: [
-        [
-          { text: '✅ Comenzar a usar COLE', callback_data: "comenzarTutorial" },
-        ],
-        [
-          { text: '👀 Conocer más sobre el proyecto', callback_data: "infoDelProyecto" },
-        ],
-        [
-          { text: '👥 Hablar con los desarrolladores', callback_data: "contacto" },
+    let buttons = {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            { text: '✅ Comenzar a usar COLE', callback_data: "comenzarTutorial" },
+          ],
+          [
+            { text: '👀 Conocer más sobre el proyecto', callback_data: "infoDelProyecto" },
+          ],
+          [
+            { text: '👥 Hablar con los desarrolladores', callback_data: "contacto" },
+          ]
         ]
-      ]
-    },
-    parse_mode: "Markdown"
-  };
+      },
+      parse_mode: "Markdown"
+    };
 
-  if (shouldEdit) {
-    ctx.editMessageText(startMessage, buttons)
-  }
-  else {
-    ctx.reply(startMessage, buttons)
+    if (shouldEdit) {
+      ctx.editMessageText(startMessage, buttons)
+    }
+    else {
+      ctx.reply(startMessage, buttons)
+    }
   }
 }
