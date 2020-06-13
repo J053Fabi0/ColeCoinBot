2b8b38755d19c1c47503d81b32d992b5e2a50340
J053Fabi0 <J053Fabi0@users.noreply.github.com>
2020-06-13 15:06:35 -0600

    test

--- src/callbackHandlers/admin/getUsers.js
+++ src/callbackHandlers/admin/getUsers.js
@@ -1,26 +1,39 @@
-const findOneUser = require('./findOneUser')
-
 module.exports = (bot, db, admins) => {
   bot.hears(/\buser/i, async (ctx, next) => {
     if (!admins.includes(ctx.update.message.from.id)) {
-      next()
-      return
+      next();
+      return;
     }
-    
+
     let user_id = Number(ctx.match.input.split(" ")[1]);
-    
-    if (isNaN(user_id)) { // Si no puse un user_id 
+
+    if (isNaN(user_id)) {
+      // Si no puse un user_id
       db.find({}, (err, doc) => {
-          let users = "";
-          doc.forEach(user => {
-            users = users + JSON.stringify(user, null, 1) + "\n"
-          })
-          ctx.reply("`" + users + "`*" + doc.length + " total users.*", { parse_mode : "Markdown" })
-        }) 
+        let users = "";
+        doc.forEach((user) => {
+          users = users + JSON.stringify(user, null, 1) + "\n";
+        });
+        ctx.reply("`" + users + "`*" + doc.length + " total users.*", {
+          parse_mode: "Markdown",
+        });
+      });
+    } else {
+      // Si puse un user_id en el texto
+      const userInfo = await db
+        .where("id", "==", user_id.toString())
+        .get()
+        .then((user) => {
+          if (user) {
+            return resolve("`" + JSON.stringify(user, null, 1) + "`", {
+              parse_mode: "Markdown",
+            });
+          } else {
+            return resolve("El usuario con ese id no existe");
+          }
+        });
+      ctx.reply(userInfo, { parse_mode: "Markdown" });
     }
-    else { // Si puse un user_id en el texto
-      ctx.reply( await findOneUser(user_id, db), {parse_mode: "Markdown"})
-    }
-  })
-  
-}
+  });
+};
+
