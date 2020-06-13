7185237d30fe1f0e5ab62271956b65d16512d23e
J053Fabi0 <J053Fabi0@users.noreply.github.com>
2020-06-13 14:45:15 -0600

    test

--- src/callbackHandlers/admin/deleteUser.js
+++ src/callbackHandlers/admin/deleteUser.js
@@ -1,21 +1,27 @@
 module.exports = (bot, db, admins) => {
-  bot.hears(/delete/i, (ctx, next) => {
+  bot.hears(/deleteUser/i, (ctx, next) => {
     if (!admins.includes(ctx.update.message.from.id)) {
-      next()
-      return
+      next();
+      return;
     }
-    
+
     let user_id = Number(ctx.match.input.split(" ")[1]);
 
     if (!isNaN(user_id)) {
-      db.doc(user_id.toString()).delete().then(function() { 
-        await ctx.reply("Se ha eliminado el usuario de la base de datos correctamente.")
-        const findOneUser = require('./findOneUser')
-        ctx.reply(await findOneUser(user_id, db))
+      db.doc(user_id.toString())
+        .delete()
+        .then(async function () {
+          await ctx.reply(
+            "Se ha eliminado el usuario de la base de datos correctamente."
+          );
+          const findOneUser = require("./findOneUser");
+          ctx.reply(await findOneUser(user_id, db));
+        });
+    } else {
+      ctx.reply("El uso del comando es `delete <user_id>`.", {
+        parse_mode: "Markdown",
       });
     }
-    else {
-      ctx.reply("El uso del comando es `delete <user_id>`.", {parse_mode: "Markdown"})
-    }
-  })
-}
+  });
+};
+
