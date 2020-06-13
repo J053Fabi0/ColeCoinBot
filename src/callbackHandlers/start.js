fc9655aac7f75fd59b9eb491bf2634737e684f8b
J053Fabi0 <J053Fabi0@users.noreply.github.com>
2020-06-12 18:17:48 -0600

    Test

--- src/callbackHandlers/start.js
+++ src/callbackHandlers/start.js
@@ -1,58 +1,66 @@
 module.exports = (bot, db) => {
-    bot.hears(/./, ctx => {
-        start(ctx, false);
-    })
-    bot.action('start', ctx => {
-        ctx.answerCbQuery();
-        start(ctx, true);
-    })
-    bot.action('volver_ayuda', ctx => {
-        ctx.deleteMessage();
-        ctx.answerCbQuery();
-        start(ctx, false);
-    })
+  bot.hears(/./, (ctx) => {
+    start(ctx, false);
+  });
+  bot.action("start", (ctx) => {
+    ctx.answerCbQuery();
+    start(ctx, true);
+  });
+  bot.action("volver_ayuda", (ctx) => {
+    ctx.deleteMessage();
+    ctx.answerCbQuery();
+    start(ctx, false);
+  });
 
-    const infoDelProyecto = require('./menu/infoDelProyecto');
-    infoDelProyecto(bot, db);
+  const infoDelProyecto = require("./menu/infoDelProyecto");
+  infoDelProyecto(bot, db);
 
-    const tutorial = require('./menu/tutorial');
-    tutorial(bot, db);
+  const tutorial = require("./menu/tutorial");
+  tutorial(bot, db);
 
-    const contacto = require('./menu/contacto');
-    contacto(bot, db);
-}
+  const contacto = require("./menu/contacto");
+  contacto(bot, db);
+};
 
 function start(ctx, shouldEdit) {
-    let chat_type = "private"
-    if (ctx.update.message != undefined) {
-        chat_type = ctx.update.message.chat.type;
-    }
-     
-    if (chat_type == "private") {
-        const { startMessage } = require('../messages/messages');
+  let chat_type = "private";
+  if (ctx.update.message != undefined) {
+    chat_type = ctx.update.message.chat.type;
+  }
 
-        let buttons = {
-            reply_markup: {
-                inline_keyboard: [
-                    [
-                        { text: '✅ Comenzar a usar COLE', callback_data: "comenzarTutorial" },
-                    ],
-                    [
-                        { text: '👀 Conocer más sobre el proyecto', callback_data: "infoDelProyecto" },
-                    ],
-                    [
-                        { text: '👥 Hablar con los desarrolladores', callback_data: "contacto" },
-                    ]
-                ]
+  if (chat_type == "private") {
+    const { startMessage } = require("../messages/messages");
+
+    let buttons = {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: "✅ Comenzar a usar COLEs",
+              callback_data: "comenzarTutorial",
             },
-            parse_mode: "Markdown"
-        };
+          ],
+          [
+            {
+              text: "👀 Conocer más sobre el proyecto",
+              callback_data: "infoDelProyecto",
+            },
+          ],
+          [
+            {
+              text: "👥 Hablar con los desarrolladores",
+              callback_data: "contacto",
+            },
+          ],
+        ],
+      },
+      parse_mode: "Markdown",
+    };
 
-        if (shouldEdit) {
-            ctx.editMessageText(startMessage, buttons)
-        }
-        else {
-            ctx.reply(startMessage, buttons)
-        }
+    if (shouldEdit) {
+      ctx.editMessageText(startMessage, buttons);
+    } else {
+      ctx.reply(startMessage, buttons);
     }
+  }
 }
