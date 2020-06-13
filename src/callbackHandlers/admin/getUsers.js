bb03454edc0de37e35ccaa3d021de4bc98e0f01d
J053Fabi0 <J053Fabi0@users.noreply.github.com>
2020-06-13 15:19:59 -0600

    test

--- src/callbackHandlers/admin/getUsers.js
+++ src/callbackHandlers/admin/getUsers.js
@@ -25,7 +25,7 @@ module.exports = (bot, db, admins) => {
         .get()
         .then((user) => {
           if (user) {
-            return "`" + (user, null, 1) + "`";
+            return "`" + (user.doc, null, 1) + "`";
           } else {
             return "El usuario con ese id no existe";
           }
