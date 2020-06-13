cd86287dfbedf069bb93c297cdd1e7b09207f02e
J053Fabi0 <J053Fabi0@users.noreply.github.com>
2020-06-13 16:41:05 -0600

    test

--- src/callbackHandlers/admin/updateValues.js
+++ src/callbackHandlers/admin/updateValues.js
@@ -25,7 +25,7 @@ module.exports = (bot, db, admins) => {
         invitations: new_value,
       })
       .then(function () {
-        await bot.telegram.sendMessage(ctx.update.message.chat.id, "Listo");
+        bot.telegram.sendMessage(ctx.update.message.chat.id, "Listo");
       })
       .catch(function (error) {
         // The document probably doesn't exist.
