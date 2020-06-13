3bb9e824425702b275615a5f5e0574981cb06b62
J053Fabi0 <J053Fabi0@users.noreply.github.com>
2020-06-13 14:48:14 -0600

    test

--- src/callbackHandlers/admin/admin.js
+++ src/callbackHandlers/admin/admin.js
@@ -1,26 +1,28 @@
 module.exports = (bot, db, admins) => {
-  const updateValues = require('./updateValues')
-  updateValues(bot, db, admins)
-  
-  const getUsers = require('./getUsers')
-  getUsers(bot, db, admins)
-  
-  const deleteUser = require('./deleteUser')
-  deleteUser(bot, db, admins)
-  
-  const addUser = require('./addUser')
-  addUser(bot, db, admins)
-  
+  const updateValues = require("./updateValues");
+  updateValues(bot, db, admins);
+
+  const getUsers = require("./getUsers");
+  getUsers(bot, db, admins);
+
+  const deleteUser = require("./deleteUser");
+  deleteUser(bot, db, admins);
+
+  const addUser = require("./addUser");
+  addUser(bot, db, admins);
+
   bot.hears(/admin/i, (ctx, next) => {
     if (!admins.includes(ctx.update.message.from.id)) {
-      next()
-      return
+      next();
+      return;
     }
-    let instrucciones = "*Borrar usuario:* `delete <user_id>`.\n" +
-        "*Añadir usuario:* `addUser <user_id> <address_103_chars_long>`.\n" + 
-        "*Cambiar datos de un usuario:* `update <address/has_invited/invitations> <user_id> <new_value>`.\n" +
-        "*Ver datos de un usuario:* `user <user_id>`.\n" +
-        "*Ver todos los usuarios:* `user`.\n"
-    ctx.reply(instrucciones, {parse_mode: "Markdown"})
-  })
-}
+    let instrucciones =
+      "*Borrar usuario:* `deleteUser <user_id>`.\n" +
+      "*Añadir usuario:* `addUser <user_id> <address_103_chars_long>`.\n" +
+      "*Cambiar datos de un usuario:* `update <address/has_invited/invitations> <user_id> <new_value>`.\n" +
+      "*Ver datos de un usuario:* `user <user_id>`.\n" +
+      "*Ver todos los usuarios:* `user`.\n";
+    ctx.reply(instrucciones, { parse_mode: "Markdown" });
+  });
+};
+
--- src/callbackHandlers/admin/deleteUser.js
+++ src/callbackHandlers/admin/deleteUser.js
@@ -11,11 +11,12 @@ module.exports = (bot, db, admins) => {
       db.doc(user_id.toString())
         .delete()
         .then(async function () {
-          await ctx.reply(
+          await bot.telegram.sendMessage(
+            ctx.update.message.chat.id,
             "Se ha eliminado el usuario de la base de datos correctamente."
           );
-          const findOneUser = require("./findOneUser");
-          ctx.reply(await findOneUser(user_id, db));
+          // const findOneUser = require("./findOneUser");
+          // ctx.reply(await findOneUser(user_id, db));
         });
     } else {
       ctx.reply("El uso del comando es `delete <user_id>`.", {
